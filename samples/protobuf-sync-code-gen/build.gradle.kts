84461f753f97fefce87e4c62972cf696c549c24a
Amin Ghadersohi <amin.ghadersohi@gmail.com>
2021-11-12 15:24:03 -0500

    update

--- samples/protobuf-sync-code-gen/build.gradle.kts
+++ samples/protobuf-sync-code-gen/build.gradle.kts
@@ -9,7 +9,7 @@ import com.google.protobuf.gradle.protoc
 plugins {
     application
     id("com.google.protobuf") version "0.8.17"
-    id("com.tinder.gitquery") version "3.0.11"
+    id("com.tinder.gitquery") version "3.0.12"
     java
 }
 
